3219197a24d4730dd1b7e06a38be7c9b56b968d8
Ömer Ertekin <57711950+OmerErtekin@users.noreply.github.com>
2020-06-03 12:53:45 +0300

    Delete features.h

--- Proje/features.h
+++
@@ -1,9 +0,0 @@
-#ifndef _FEATURES_H_
-#define _FEATURES_H_
-
-#include "types.h"
-
-void cleanup(char *s);
-double sim_score(struct features *s1, struct features *s2);
-void compute_features(char *text, struct features *feat);
-#endif
